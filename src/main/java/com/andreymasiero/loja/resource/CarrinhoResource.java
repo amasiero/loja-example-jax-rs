4bc734d644670412915811142474ce14b37871fd
Andrey Masiero <andreymasiero@gmail.com>
2020-03-22 15:02:08 -0300

    Added post methos to REST Service

--- src/main/java/com/andreymasiero/loja/resource/CarrinhoResource.java
+++ src/main/java/com/andreymasiero/loja/resource/CarrinhoResource.java
@@ -1,13 +1,19 @@
 package com.andreymasiero.loja.resource;
 
+import java.net.URI;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import com.andreymasiero.loja.dao.CarrinhoDAO;
 import com.andreymasiero.loja.model.Carrinho;
+import com.thoughtworks.xstream.XStream;
 
 @Path("/carrinhos")
 public class CarrinhoResource {
@@ -18,5 +24,15 @@ public class CarrinhoResource {
 	public String busca(@PathParam("id") long id) {
 		Carrinho carrinho = new CarrinhoDAO().busca(id);
 		return carrinho.toXML();
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_XML)
+	@Produces(MediaType.APPLICATION_XML)
+	public Response adiciona(String conteudo) {
+		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+		new CarrinhoDAO().adiciona(carrinho);
+		URI uri = URI.create("/loja/carrinhos/" + carrinho.getId());
+		return Response.created(uri).build();
 	}
 }
--- src/test/java/com/andreymasiero/loja/test/CarrinhoTest.java
+++ src/test/java/com/andreymasiero/loja/test/CarrinhoTest.java
@@ -2,10 +2,16 @@ package com.andreymasiero.loja.test;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.junit.Assert;
 import org.junit.Test;
+
+import com.andreymasiero.loja.model.Carrinho;
+import com.andreymasiero.loja.model.Produto;
 
 public class CarrinhoTest {
 	
@@ -13,8 +19,30 @@ public class CarrinhoTest {
 	public void testeCarrinhoExistenteNoBanco() {
 		Client client = ClientBuilder.newClient();
 		WebTarget target = client.target("http://localhost:8080/loja");
-		String conteudo = target.path("/carrinhos").request().get(String.class);
+		String conteudo = target.path("/carrinhos/1").request().get(String.class);
 		Assert.assertTrue(conteudo.contains("Lins de Vasconcelos"));
+	}
+	
+	@Test
+	public void testeCarrinhoCriadoViaPost() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8080/loja");
+		
+		Carrinho carrinho = new Carrinho();
+		carrinho.adiciona(new Produto(3456l, "Mouse Gamer", 120, 1));
+		carrinho.setRua("Avenida Lins de Vasconcelos 1222, 10 andar");
+		carrinho.setCidade("São Paulo");
+		
+		String xml = carrinho.toXML();
+		
+		Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
+		
+		Response response = target.path("/carrinhos").request().post(entity);
+		Assert.assertEquals(201, response.getStatus());
+		
+		String location = response.getHeaderString("Location");
+		String conteudo = client.target(location).request().get(String.class);
+		Assert.assertTrue(conteudo.contains("Mouse"));
 	}
 
 }
