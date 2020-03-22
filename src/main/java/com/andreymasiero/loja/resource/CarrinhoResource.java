306a3ca254d084880c250d9bd5cf9b419157ebeb
Andrey Masiero <andreymasiero@gmail.com>
2020-03-21 23:28:58 -0300

    Using parameters

--- src/main/java/com/andreymasiero/loja/resource/CarrinhoResource.java
+++ src/main/java/com/andreymasiero/loja/resource/CarrinhoResource.java
@@ -2,6 +2,7 @@ package com.andreymasiero.loja.resource;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -11,10 +12,11 @@ import com.andreymasiero.loja.model.Carrinho;
 @Path("/carrinhos")
 public class CarrinhoResource {
 
+	@Path("/{id}")
 	@GET
 	@Produces(MediaType.APPLICATION_XML)
-	public String busca() {
-		Carrinho carrinho = new CarrinhoDAO().busca(1l);
+	public String busca(@PathParam("id") long id) {
+		Carrinho carrinho = new CarrinhoDAO().busca(id);
 		return carrinho.toXML();
 	}
 }
